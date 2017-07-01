2aa3d135373616ab3008f097cb567e8fd1683168
JRempel <Jonathan.Rempel@dal.ca>
2017-07-01 20:29:23 -0300

    Added SCAN unit tests, fixed SCAN direction bug

--- src/main/java/algorithms/SCAN.java
+++ src/main/java/algorithms/SCAN.java
@@ -1,23 +1,33 @@
 package algorithms;
 
+import static algorithms.SCAN.Direction.DOWN;
+import static algorithms.SCAN.Direction.UP;
+
 /**
  * The SCAN algorithm selects requests that are nearest to the current head position and in
  * the direction the head is traveling. When it hits the maximum or minimum cylinder, it
  * repeats the process while moving in the opposite direction.
  */
 public class SCAN extends AbstractDiskScheduler {
-    private enum Direction {
+    enum Direction {
         UP, DOWN
     }
+
+    private Direction currentDirection;
+
+    public SCAN() {
+        super();
+        currentDirection = UP;
+    }
+
     public void run() {
         Integer nextRequest = 0;
-        Direction direction = Direction.UP;
         while (!requestQueue.isEmpty()) {
-            switch (direction) {
+            switch (currentDirection) {
                 case UP:
                     nextRequest = getNextRequestUp();
                     if (nextRequest == null) {
-                        direction = Direction.DOWN;
+                        currentDirection = DOWN;
                         totalHeadMovements += (MAX_DISK_CYLINDER - currentHeadCylinder) * 2;
                         nextRequest = getNextRequestDown();
                     }
@@ -25,7 +35,7 @@ public class SCAN extends AbstractDiskScheduler {
                 case DOWN:
                     nextRequest = getNextRequestDown();
                     if (nextRequest == null) {
-                        direction = Direction.UP;
+                        currentDirection = UP;
                         totalHeadMovements += (currentHeadCylinder - MIN_DISK_CYLINDER) * 2;
                         nextRequest = getNextRequestUp();
                     }
--- src/test/java/algorithms/SCANTest.java
+++ src/test/java/algorithms/SCANTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.assertTrue;
 
 public class SCANTest {
     @Test
-    public void testSCAN() {
+    public void testSCANBounceTop() {
         AbstractDiskScheduler scheduler = new SCAN();
         ArrayList<Integer> queue = new ArrayList<Integer>() {{
             add(40);
@@ -30,6 +30,40 @@ public class SCANTest {
                 315, scheduler.totalHeadMovements);
         assertEquals("Expected final currentHeadCylinder to be equal 35.",
                 35, scheduler.currentHeadCylinder);
+        assertEquals("Expected the order processed to be equal to the test expected order.",
+                expectedOrder, scheduler.orderProcessed);
+        assertTrue("Expected request queue to be empty after processing.", scheduler.requestQueue.isEmpty());
+    }
+
+    @Test
+    public void testSCANBounceBottom() {
+        AbstractDiskScheduler scheduler = new SCAN();
+        ArrayList<Integer> queue = new ArrayList<Integer>() {{
+            add(40);
+            add(100);
+            add(35);
+        }};
+        ArrayList<Integer> expectedOrder = new ArrayList<Integer>() {{
+            add(100);
+            add(40);
+            add(35);
+            add(50);
+        }};
+        scheduler.setRequestQueue(queue);
+        scheduler.currentHeadCylinder = 200;
+
+        scheduler.run();
+
+        queue.clear();
+        queue.add(50);
+        scheduler.setRequestQueue(queue);
+
+        scheduler.run();
+
+        assertEquals("Expected total head movements to equal 250.",
+                250, scheduler.totalHeadMovements);
+        assertEquals("Expected final currentHeadCylinder to be equal 50.",
+                50, scheduler.currentHeadCylinder);
         assertEquals("Expected the order processed to be equal to the test expected order.",
                 expectedOrder, scheduler.orderProcessed);
         assertTrue("Expected request queue to be empty after processing.", scheduler.requestQueue.isEmpty());
