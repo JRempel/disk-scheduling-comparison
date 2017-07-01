53fc1db735ec583917ae7d354da0585dfdb466e6
JRempel <Jonathan.Rempel@dal.ca>
2017-07-01 16:32:22 -0300

    Add SCAN description, actually make SCAN instead of C-SCAN

--- src/main/java/algorithms/SCAN.java
+++ src/main/java/algorithms/SCAN.java
@@ -1,7 +1,9 @@
 package algorithms;
 
 /**
- *
+ * The SCAN algorithm selects requests that are nearest to the current head position and in
+ * the direction the head is traveling. When it hits the maximum or minimum cylinder, it
+ * repeats the process while moving in the opposite direction.
  */
 public class SCAN extends AbstractDiskScheduler {
     private enum Direction {
@@ -16,6 +18,7 @@ public class SCAN extends AbstractDiskScheduler {
                     nextRequest = getNextRequestUp();
                     if (nextRequest == null) {
                         direction = Direction.DOWN;
+                        totalHeadMovements += (MAX_DISK_CYLINDER - currentHeadCylinder) * 2;
                         nextRequest = getNextRequestDown();
                     }
                     break;
@@ -23,6 +26,7 @@ public class SCAN extends AbstractDiskScheduler {
                     nextRequest = getNextRequestDown();
                     if (nextRequest == null) {
                         direction = Direction.UP;
+                        totalHeadMovements += (currentHeadCylinder - MIN_DISK_CYLINDER) * 2;
                         nextRequest = getNextRequestUp();
                     }
                     break;
